d256f921991f48675ca7db8d846d8b4795cde7a1
FrankLikuohao <frank.likuohao@gmail.com>
2017-05-23 20:53:52 +0800

    第一講修正

--- Lecture01-Examples/Example01/Program.cs
+++ Lecture01-Examples/Example01/Program.cs
@@ -11,6 +11,7 @@ namespace Example01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+            Console.WriteLine("Just Try");
         }
     }
 }
