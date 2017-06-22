b764453234d822013c3eb643685a3f74b905061b
yolanda109 <yolanda1983109@hotmail.com>
2017-06-22 23:17:24 +0800

    新增類別範例

---
+++ Lecture01-Examples/Example02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("你的名字:{0}",
+                Console.ReadLine());
+        }
+    }
+}
---
+++ Lecture01-Examples/Example03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student someone = new Student();
+            someone.name = "陳雅姿";
+            Console.WriteLine("學生的名字:{0}", someone.name);
+
+            someone.birthday = "1009";
+            Console.WriteLine("學生的生日:{0}", someone.birthday);
+
+            someone.email = "abc@email.com";
+            Console.WriteLine("學生的信箱:{0}", someone.email);
+
+            someone.phone = "0123456789";
+            Console.WriteLine("學生的電話:{0}", someone.phone);
+
+            someone.age = 5;
+            Console.WriteLine("學生的年齡:{0}", someone.age);
+        }
+    }
+}
